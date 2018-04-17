3b0f4e9ee15d56b77d26892308a569fd43412db0
therafk <therafk@hotmail.com>
2018-04-17 12:03:57 +0100

    Getting Started with Entity Framework 6 Code First using MVC 5

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }       //foreign key
+        public int StudentID { get; set; }      //foreign key
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
